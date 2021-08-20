50bbf741c701fa99e099ef0f5f00dccd1b75b9f1
nhamilton1 <nickh3592@gmail.com>
2021-08-20 11:56:58 -0400

    I see why i am failing my last time, trying to figure out how to fix it now

--- src/components/UserOrder.js
+++ src/components/UserOrder.js
@@ -1,20 +1,20 @@
-import React from 'react'
+// import React from 'react'
 
-export default function UserOrder (props) {
-    const { user } = props
-    return (
-        <div className='friend container'>
-            <h2>{user.name}</h2>
-            <p>Pizza Size: {user.size}</p>
-            {
-            <div>
-            toppings:
-                <ul>
-                    {user.toppings.map((like, idx) => <li key={idx}>{like}</li>)}
-                </ul>
-            </div>
-            }
-            <p>{user.instructions}</p>
-        </div>
-    )
-}
+// export default function UserOrder (props) {
+//     const { user } = props
+//     return (
+//         <div className='friend container'>
+//             <h2>{user.name}</h2>
+//             <p>Pizza Size: {user.size}</p>
+//             {
+//             <div>
+//             toppings:
+//                 <ul>
+//                     {user.toppings.map((like, idx) => <li key={idx}>{like}</li>)}
+//                 </ul>
+//             </div>
+//             }
+//             <p>{user.instructions}</p>
+//         </div>
+//     )
+// }
