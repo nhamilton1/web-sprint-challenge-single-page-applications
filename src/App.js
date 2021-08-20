a85b076efcbed2b043033ccb1d5ae7fecd4dd765
nhamilton1 <nickh3592@gmail.com>
2021-08-20 08:54:14 -0400

    starting the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import OrderForm from './components/OrderForm'
+
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  name: '',
+  instructions: '',
+  ///// DROPDOWN /////
+  size: '',
+  ///// CHECKBOXES /////
+  cheese: false,
+  sausage: false,
+  peppers: false,
+  pepperoni: false,
+}
+
+// const initialFormErrors = {
+//   ///// TEXT INPUTS /////
+//   name: '',
+//   instructions: '',
+//   ///// DROPDOWN /////
+//   size: '',
+
+// }
+
+const initialUsers = []
+// const initialDisabled = true
 
 const App = () => {
+
+  const [ user, setUser ] = useState(initialUsers)
+  const [ formValues, setFormValues ] = useState(initialFormValues)
+  // const [ formErrors, setFormErrors ] = useState(initialFormErrors)
+  // const [ disabled, setDisabled ] = useState(initialDisabled)
+
+
+  const postNewUser = newFriend => {
+    axios.post('https://reqres.in/api/orders', newFriend)
+      .then(res => {
+        setUser([res.data, ...user]);
+      }).catch(err => console.error(err));
+
+    setFormValues(initialFormValues);
+  }
+
+
+  const inputChange = (name, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value // NOT AN ARRAY
+    })
+  }
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      instructions: formValues.instructions.trim(),
+      size: formValues.size.trim(),
+      toppings: ['cheese', 'sausage', 'pepers', 'pepperoni'].filter(toppings => !!formValues[toppings]),
+    }
+    postNewUser(newUser)
+  }
+
+
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='App'>
+      <nav>
+        <h1 className='store-header'>Lambda Eats</h1>
+        <div className='nav-links'>
+          <Link to="/">Home</Link>
+          <Link to="/pizza">Order Pizza</Link>
+        </div>
+      </nav>
+      <OrderForm 
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+      />
+    </div>
   );
 };
 export default App;
---
+++ src/components/OrderForm.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+export default function OrderForm(props) {
+    const { values, submit, change } = props
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+      }
+
+      const onChange = evt => {
+        /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
+        const { name, value, checked, type } = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse);
+      }
+
+    return(
+        <form id="pizza-form" onSubmit={onSubmit}>
+            <label>
+                Name:
+                <input 
+                    value={values.name}
+                    onChange={onChange}
+                    name='name'
+                    type='text'  
+                    id='name-input'
+                />
+            </label>
+            <label>
+                Select your pizza size:
+                <select 
+                    onChange={onChange}
+                    value={values.toppings}
+                    name='size'
+                    id='size-dropdown'
+                >
+                    <option value=''>- Select a Size -</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='large'>Large</option>
+                    <option value='XL'>Extra Large</option>
+                </select>
+            </label>
+            <label>
+                Cheese
+                <input 
+                    type="checkbox"
+                    name="cheese"
+                    checked={values.cheese}
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                sausage
+                <input 
+                    type="checkbox"
+                    name="sausage"
+                    checked={values.sausage}
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                peppers
+                <input 
+                    type="checkbox"
+                    name="peppers"
+                    checked={values.peppers}
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                pepperoni
+                <input 
+                    type="checkbox"
+                    name="pepperoni"
+                    checked={values.pepperoni}
+                    onChange={onChange}
+                />
+            </label>
+        </form>
+    )
+}
